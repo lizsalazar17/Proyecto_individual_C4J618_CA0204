eb496dc73a60b6102cf00b841514c4a0d3218f9c
LizSalazar17 <lizsalazara7@gmail.com>
2025-11-04 23:19:23 -0600

    feat: Cálculo de equilibrio y test chi cuadrado del gen o

--- cod/Cálculo_de_frecuencias_alelicas.R
+++ cod/Cálculo_de_frecuencias_alelicas.R
@@ -22,12 +22,14 @@ df.hembras <- df %>%
 
 #Veamos el total de alelos O en los machos
 total.machos.O <- sum(df.machos$Genotipo_O == "x^Oy")
+total.machos.o <- sum(df.machos$Genotipo_O == "x^oy")
 
 total.alelos.machos <- nrow(df.machos)
 
 #Loa alelos donde se presenta x^Oy^O lo multiplicamos por 2 ya que se encuentran 2 alelos del gen O
-total.hembras.O.O <- sum(df.hembras$Genotipo_O == "x^Oy^O") * 2
+total.hembras.O.O <- sum(df.hembras$Genotipo_O == "x^Ox^O") * 2
 total.hembras.O.o <- sum(df.hembras$Genotipo_O == "x^Ox^o")
+total.hembras.o.o <- sum(df.hembras$Genotipo_O == "x^ox^o")
 total.alelos.hembras <- nrow(df.hembras)
 
 total.hembras.O <- total.hembras.O.O + total.hembras.O.o
@@ -43,4 +45,154 @@ frecuencia.o <- 1 - frecuencia.O
 
 frecuencia.O
 frecuencia.o
+
+#Veamos el equilibrio del gen o 
+#Note que como los machos solo necesitan un cromosoma X, que es el que hereda el gen del color naranja, la probabilidad sería nada más p + q = 1. 
+#Por lo tanto, se toma la variable frecuencia. O = p y la variable frecuencia.o = q. 
+
+#Entonces, para machos se esperaría: 
+
+machos.esperado.O <- total.alelos.machos * frecuencia.O
+
+machos.esperado.o <- total.alelos.machos * frecuencia.o
+
+machos.esperado.O
+
+#Para el caso de la hembras, si necesitamos usar la ecuación completa de Handy-Weinberg donde la probabilidad se vería así: 
+#p ^ 2 + 2pq + q ^2
+#Entonces, el valor esperado de las hembras con el alelo O es de: 
+
+hembras.esperado.OO <- total.alelos.hembras * (frecuencia.O) * (frecuencia.O)
+
+hembras.esperado.oo <- total.alelos.hembras * (frecuencia.o) * (frecuencia.o)
+
+hembras.esperado.Oo <- total.alelos.hembras * 2 * (frecuencia.O) * (frecuencia.o)
+
+#Comparemos el resultado con lo observado antes
+
+total.hembras.O.o
+hembras.esperado.Oo
+
+total.hembras.o.o
+hembras.esperado.oo
+
+total.hembras.O.O
+hembras.esperado.OO
+
+total.machos.O
+machos.esperado.O
+
+total.machos.o
+machos.esperado.o
+
+#Utilicemos chi cuadrado para ver que tanto cambio el valor esperado con el valor observado. 
+
+
+chi.cuadrado <- function(valor.esperado, valor.observado){ 
+      sum((valor.observado - valor.esperado)^2/valor.esperado) 
+  } 
+#Para calcular el chi cuadrado de los machos 
+
+vector.esperado.m<- c(machos.esperado.O, machos.esperado.o)
+
+vector.observado.m <- c(total.machos.O, total.machos.o) 
+
+chi.machos <- chi.cuadrado(vector.esperado.m, vector.observado.m) 
+
+chi.machos
+
+p.val.m <- 1 - pchisq(chi.machos, 1)
+
+p.val.m 
+#Para calcular el chi cuadrado de las hembras 
+vector.esperado.h <- c(hembras.esperado.OO, hembras.esperado.Oo, hembras.esperado.oo)
+
+vector.observado.h <- c(total.hembras.O.O, total.hembras.O.o, total.hembras.o.o) 
+
+chi.hembras <- chi.cuadrado(vector.esperado.h, vector.observado.h) 
+
+chi.hembras
+
+p.val.h <- 1 - pchisq(chi.hembras, 2) 
+p.val.h
+
+#Veamos los resultados de maner más ordenada
+tibble(
+  Categoria = c("X^OY", "X^oY", "X^OX^O", "X^OX^o", "X^oX^o"),
+  Observado = c(total.machos.O, total.machos.o,
+                total.hembras.O.O, total.hembras.O.o, total.hembras.o.o),
+  Esperado = c(machos.esperado.O, machos.esperado.o,
+               hembras.esperado.OO, hembras.esperado.Oo, hembras.esperado.oo),
+  Diferencia = Observado - Esperado
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
