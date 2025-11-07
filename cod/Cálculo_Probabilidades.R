9b16b76e21492962a7ac46b9b89e0e9f3bee7664
LizSalazar17 <lizsalazara7@gmail.com>
2025-11-07 15:29:50 -0600

    fix: arreglo de método de simulación teórica

--- cod/Cálculo_Probabilidades.R
+++ cod/Cálculo_Probabilidades.R
@@ -1,12 +1,19 @@
 #Vamos a definir una simulación teórica siguiendo el equilibrio de Handy Weinberg encontrado anteriormente.
 
 library(tidyverse)
-library(ggpplot2)
+library(ggplot2)
 
 #Definamos p y q, donde p es la probabilidad de que el gen porte el color naranja y q de que no. 
 
 p <- frecuencia.O
 q <- 1 - frecuencia.o
+
+
+#Probabilidad para las hembras (ya que poseen dos alelos)
+
+p.O.O <- p^2
+p.O.o <- 2* p * q
+p.o.o <- q^2
 
 
 #Armaremos 100 parejas y cada pareja tendrá 100 crías para ver a qué convergería normalmente el genotipo de distintos gatos. 
@@ -21,29 +28,35 @@ simulacion.teorica <- function(n_parejas, n_crias, p, q){
   
   #Generamos machos simulados
   
-  machos.simulados <- sample(
+  machos.simulados = sample(
     x = c("x^Oy", "x^oy"), 
-    tamanio = n_parejas, 
+    size = n_parejas, 
     replace = TRUE, 
-    probabilidad = c(p,q)
+    prob = c(p,q)
   )
   
-  #Probabilidad para las hembras (ya que poseen dos alelos)
-  
-  p.O.O <- p^2
-  p.O.o <- 2* p * q
-  p.o.o <- q^2
-  
-  
-  
   #Generamos hembras simuladas
-  hembras.simuladas <- sample(
+  hembras.simuladas = sample(
     x = c("x^Ox^O", "x^Ox^o", "x^ox^o"),
-    tamanio = n_parejas, 
+    size = n_parejas, 
     replace = TRUE,
     prob = c(p.O.O, p.O.o, p.o.o)
   )
   
   #Ahora cruzamos las parejas
   
+  total.crias = character(0)
+  
+  for (i in seq_len(n.parejas)) {
+    crias.i = cruce.gen.O(machos.simulados[i], hembras.simuladas[i], n.crias)
+    total.crias = c(total.crias, crias.i)
+  }
+  
+  tibble(Genotipo = total.crias) |>
+    count(Genotipo, name = "Frecuencia") |>
+    mutate(Proporcion = Frecuencia / sum(Frecuencia))
 }
+  
+resultados.teorico <- simulacion.teorica(n.parejas, n.crias, p, q)
+
+resultados.teorico
