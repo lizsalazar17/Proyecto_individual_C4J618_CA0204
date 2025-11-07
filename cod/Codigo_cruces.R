cccbe47d13cccb637261825e56816d64e31acd88
LizSalazar17 <lizsalazara7@gmail.com>
2025-11-07 15:29:22 -0600

    feat: creación de función para cruzar gatos

---
+++ cod/Codigo_cruces.R
@@ -0,0 +1,40 @@
+#Hagamos una función que nos ayuda ver la posible descendencia entre dos gatos
+
+#Esta función tomará los geneotipos del padre y la madre, y la cantidad de crías que tendrán. 
+cruce.gen.O <- function(padre.genotipo, madre.genotipo, n.gatitos){
+  #Para filtrar los alelos del padre
+  alelo.padre = gsub("x\\^", "", padre.genotipo |> gsub("y", "", x = _))
+  
+  alelos.madre = strsplit(gsub("x\\^", "", madre.genotipo), "x")[[1]]
+  
+  #Para limpiar si no queda algún "" de más 
+  
+  alelos.madre = alelos.madre[alelos.madre != ""]
+  
+  descendencia = character(n.gatitos)
+  
+    for (i in seq_len(n.gatitos)) {
+    #Sabemos que la madre aporta uno de sus dos cromosomas X
+    
+      alelo.madre = sample(alelos.madre, 1)
+    
+      #La probabilidad de que sea hembra o macho depende de un 50%
+    
+      sexo = sample(c("Macho", "Hembra"), 1)
+      if (sexo == "Macho"){
+      #El padre le hereda el cromosoma Y al hijo independientemente de que genotipo tenga.
+      descendencia[i] = paste0("x^", alelo.madre, "y")
+      } else {
+      
+      #usamos sort para ordenar el alelo del padre primero y luego el de la madre
+      alelos.h <- sort(c(alelo.padre, alelo.madre), decreasing = TRUE)
+      descendencia[i] = paste0("x^", alelos.h[1], "x^", alelos.h[2])
+      
+      }
+      
+    }
+  
+  descendencia
+    
+  }
+  
